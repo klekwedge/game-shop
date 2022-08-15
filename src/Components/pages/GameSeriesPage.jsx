92a794a24b377d4b375659b4c00d1bfaa417fa61
klekkwedge <antonov.andrey.10101@gmail.com>
2022-08-15 16:32:18 +0300

    refactor: replace tailwindcss with Chakra UI

--- src/Components/pages/GameSeriesPage.jsx
+++ src/Components/pages/GameSeriesPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { FiExternalLink } from 'react-icons/fi';
 import { MdKeyboardBackspace } from 'react-icons/md';
+import {
+  Flex, Heading, List, ListItem,
+} from '@chakra-ui/react';
 import RAWG from '../../services/RAWG';
 import {
   gameSeriesFetching,
@@ -29,44 +31,76 @@ function GameSeriesPage() {
   }, [gameId]);
 
   return (
-    <main className="flex flex-col justify-between items-baseline gap-3 max-w-screen-2xl mx-auto px-5 pt-16 pb-2">
-      <div className="flex justify-between items-center mb-8">
-        <h2 className="text-2xl self-end ease-in duration-200 hover:text-violet-700">
+    <Flex
+      as="main"
+      flexDirection="column"
+      justifyContent="space-between"
+      maxWidth="1400px"
+      margin="0 auto"
+      padding="50px 20px 10px 20px"
+    >
+      <Flex justifyContent="space-between" alignItems="center" mb="30px">
+        <Heading
+          as="h2"
+          fontSize="32px"
+          fontWeight="500"
+          transition="all 0.3s ease"
+          _hover={{ color: '#6d28d9' }}
+        >
           <Link to="/" className="flex gap-2 items-center">
             <MdKeyboardBackspace size="40" />
             Store
           </Link>
-        </h2>
-      </div>
+        </Heading>
+      </Flex>
       {gamesOfSeries && gamesOfSeries.count > 0 ? (
         <div>
-          <h2 className="text-4xl mb-8 font-medium">
+          <Heading fontSize="40px" mb="32px" fontWeight="500">
             List of games that are part of the same series:
             {` ${gamesOfSeries.count}`}
-          </h2>
-          <ul className="flex flex-wrap gap-5">
+          </Heading>
+          <List display="flex" flexWrap="wrap" gap="20px">
             {gamesOfSeries.results.map((gameItem) => (
-              <li
+              <ListItem
                 key={gameItem.id}
-                className="flex flex-col basis-1/5 rounded-lg gap-2 bg-zinc-700 hover:scale-105 duration-300 relative"
+                display="flex"
+                flexDirection="column"
+                borderRadius="8px"
+                gap="10px"
+                flex="1 1 20%"
+                maxWidth="360px"
+                backgroundColor="#3f3f46"
+                transition="all 0.4s ease"
+                _hover={{ transform: 'scale(1.05)' }}
               >
                 <img
                   src={gameItem.background_image}
                   alt={gameItem.background_image}
-                  className="object-cover max-h-40"
+                  objectFit="cover"
+                  // className="object-cover"
                 />
-                <h3 className="self-center px-2 pb-2">{gameItem.name}</h3>
-                <Link to={`/${gameItem.id}`} className="absolute top-1 left-1">
-                  <FiExternalLink size="25" className="bg-neutral-900 rounded-lg p-1" />
-                </Link>
-              </li>
+                <Heading
+                  as="h3"
+                  fontWeight="500"
+                  fontSize="20px"
+                  textAlign="center"
+                  alignSelf="center"
+                  padding="0px 10px 5px 10px"
+                  transition="all 0.3s ease"
+                  _hover={{ color: '#d4d4d4' }}
+                >
+                  <Link to={`/${gameItem.id}`}>{gameItem.name}</Link>
+                </Heading>
+              </ListItem>
             ))}
-          </ul>
+          </List>
         </div>
       ) : (
-        <h2 className="text-4xl mb-8 font-medium">This game is not part of any series.</h2>
+        <Heading as="h2" fontSize="40px" mb="30px" fontWeight="500">
+          This game is not part of any series.
+        </Heading>
       )}
-    </main>
+    </Flex>
   );
 }
 
