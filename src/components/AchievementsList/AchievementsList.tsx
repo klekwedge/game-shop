2e53df9a2005182e9c746bc7317fab68b86df8f9
Andrey <antonov.andrey.10101@gmail.com>
2023-09-04 16:25:59 +0300

    feat: change the display conditions

--- src/components/AchievementsList/AchievementsList.tsx
+++ src/components/AchievementsList/AchievementsList.tsx
@@ -39,62 +39,60 @@ function AchievementsList({ achievementsAmount }: { achievementsAmount: number }
 
   return (
     <section>
-      {achievements.length > 0 ? (
-        <Flex flexDirection="column" alignItems="center" pt="20px">
-          <Heading as="h4" fontWeight="600" fontSize="30px" mb="40px" alignSelf="flex-start">
-            Achievements {`(${achievementsAmount})`}
-          </Heading>
-          <List display="flex" justifyContent="center" gap="35px" flexWrap="wrap" mb="60px">
-            {achievements.map((achievementItem) => (
-              <ListItem
-                key={uuidv4()}
-                flex="1 1 17%"
-                minH="100%"
-                minW="230px"
-                borderRadius="10px"
-                display="flex"
-                flexDirection="column"
-                bg="#202020"
-                className={cn({
-                  LegendaryAchievement: +achievementItem.percent <= 5,
-                  EpicAchievement: +achievementItem.percent <= 10 && +achievementItem.percent > 5,
-                  RareAchievement: +achievementItem.percent > 10 && +achievementItem.percent <= 15,
-                })}
-              >
-                <Image borderRadius="10px" src={achievementItem.image} objectFit="cover" w="100%" mb="10px" />
-                <Box p="10px">
-                  <Heading as="h4" textAlign="center" fontWeight="500" fontSize="20px" p="0px 10px 10px">
-                    {achievementItem.name}
-                    <span
-                      className={cn({
-                        LegendaryAchievementColor: +achievementItem.percent <= 5,
-                        EpicAchievementColor: +achievementItem.percent <= 10 && +achievementItem.percent > 5,
-                        RareAchievementColor: +achievementItem.percent > 10 && +achievementItem.percent <= 15,
-                      })}
-                    >
-                      {' '}
-                      ({achievementItem.percent}%)
-                    </span>
-                  </Heading>
-                  <Text textAlign="center" fontWeight="400" fontSize="16px">
-                    {achievementItem.description}
-                  </Text>
-                </Box>
-              </ListItem>
-            ))}
-          </List>
-          <Button
-            m="0 auto"
-            bg="purple.600"
-            _hover={{ bg: 'purple.700' }}
-            _active={{ bg: 'purple.500' }}
-            onClick={() => loadMoreAchievements()}
-            display={nextAchievementsPage === null ? 'none' : 'block'}
-          >
-            Load more
-          </Button>
-        </Flex>
-      ) : null}
+      <Flex flexDirection="column" alignItems="center" pt="20px">
+        <Heading as="h4" fontWeight="600" fontSize="30px" mb="40px" alignSelf="flex-start">
+          Achievements {`(${achievementsAmount})`}
+        </Heading>
+        <List display="flex" justifyContent="center" gap="35px" flexWrap="wrap" mb="60px">
+          {achievements.map((achievementItem) => (
+            <ListItem
+              key={uuidv4()}
+              flex="1 1 17%"
+              minH="100%"
+              minW="230px"
+              borderRadius="10px"
+              display="flex"
+              flexDirection="column"
+              bg="#202020"
+              className={cn({
+                LegendaryAchievement: +achievementItem.percent <= 5,
+                EpicAchievement: +achievementItem.percent <= 10 && +achievementItem.percent > 5,
+                RareAchievement: +achievementItem.percent > 10 && +achievementItem.percent <= 15,
+              })}
+            >
+              <Image borderRadius="10px" src={achievementItem.image} objectFit="cover" w="100%" mb="10px" />
+              <Box p="10px">
+                <Heading as="h4" textAlign="center" fontWeight="500" fontSize="20px" p="0px 10px 10px">
+                  {achievementItem.name}
+                  <span
+                    className={cn({
+                      LegendaryAchievementColor: +achievementItem.percent <= 5,
+                      EpicAchievementColor: +achievementItem.percent <= 10 && +achievementItem.percent > 5,
+                      RareAchievementColor: +achievementItem.percent > 10 && +achievementItem.percent <= 15,
+                    })}
+                  >
+                    {' '}
+                    ({achievementItem.percent}%)
+                  </span>
+                </Heading>
+                <Text textAlign="center" fontWeight="400" fontSize="16px">
+                  {achievementItem.description}
+                </Text>
+              </Box>
+            </ListItem>
+          ))}
+        </List>
+        <Button
+          m="0 auto"
+          bg="purple.600"
+          _hover={{ bg: 'purple.700' }}
+          _active={{ bg: 'purple.500' }}
+          onClick={() => loadMoreAchievements()}
+          display={nextAchievementsPage === null ? 'none' : 'block'}
+        >
+          Load more
+        </Button>
+      </Flex>
     </section>
   );
 }
--- src/components/AdditionsList/AdditionsList.tsx
+++ src/components/AdditionsList/AdditionsList.tsx
@@ -18,40 +18,38 @@ function AdditionsList() {
 
   return (
     <section>
-      {additions ? (
-        <List className="flex gap-5 flex-wrap" p="20px 0px">
-          {additions.map((additionItem: IAddition) => (
-            <ListItem
-              flex="1 1 25%"
-              maxW="256px"
-              maxH="144px"
-              className="flex flex-col items-center gap-2 bg-zinc-900 pb-2 rounded-lg hover:scale-105 duration-300"
-              key={additionItem.id}
+      <List className="flex gap-5 flex-wrap" p="20px 0px">
+        {additions.map((additionItem: IAddition) => (
+          <ListItem
+            flex="1 1 25%"
+            maxW="256px"
+            maxH="144px"
+            className="flex flex-col items-center gap-2 bg-zinc-900 pb-2 rounded-lg hover:scale-105 duration-300"
+            key={additionItem.id}
+          >
+            <Image
+              src={additionItem.background_image}
+              alt={additionItem.background_image}
+              objectFit="cover"
+              w="100%"
+              h="100%"
+              mb="10px"
+            />
+            <Heading
+              as="h4"
+              fontWeight="500"
+              fontSize="16px"
+              textAlign="center"
+              alignSelf="center"
+              padding="0px 10px"
+              transition="all 0.3s ease"
+              _hover={{ color: '#d4d4d4' }}
             >
-              <Image
-                src={additionItem.background_image}
-                alt={additionItem.background_image}
-                objectFit="cover"
-                w="100%"
-                h="100%"
-                mb="10px"
-              />
-              <Heading
-                as="h4"
-                fontWeight="500"
-                fontSize="16px"
-                textAlign="center"
-                alignSelf="center"
-                padding="0px 10px"
-                transition="all 0.3s ease"
-                _hover={{ color: '#d4d4d4' }}
-              >
-                <Link to={`/${additionItem.id}`}>{additionItem.name}</Link>
-              </Heading>
-            </ListItem>
-          ))}
-        </List>
-      ) : null}
+              <Link to={`/${additionItem.id}`}>{additionItem.name}</Link>
+            </Heading>
+          </ListItem>
+        ))}
+      </List>
     </section>
   );
 }
