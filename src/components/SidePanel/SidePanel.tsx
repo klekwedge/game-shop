91e4f920223ec47c93c061dec9577befa40cb4ec
Andrey <antonov.andrey.10101@gmail.com>
2023-09-04 18:09:01 +0300

    feat: change the styles for SidePanel

--- src/components/SidePanel/SidePanel.tsx
+++ src/components/SidePanel/SidePanel.tsx
@@ -2,7 +2,18 @@ import { useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { MdFamilyRestroom, MdPerson } from 'react-icons/md';
 import { NavLink, Link } from 'react-router-dom';
-import { Flex, Box, Drawer, DrawerBody, DrawerFooter, DrawerOverlay, DrawerContent, DrawerCloseButton, useDisclosure, Button } from '@chakra-ui/react';
+import {
+  Flex,
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react';
 import { IoExtensionPuzzleSharp, IoCarSport, IoSchool } from 'react-icons/io5';
 import { FaFistRaised, FaHorseHead, FaMountain, FaSnowboarding, FaChessBoard, FaTruck } from 'react-icons/fa';
 import { SiApplearcade } from 'react-icons/si';
@@ -18,9 +29,10 @@ import {
 import { useAppSelector, useAppDispatch } from '../../hooks/useRedux';
 import RAWG from '../../services/RAWG';
 import { fetchGenres } from '../../slices/genresSlice/genresSlice';
+import './SidePanel.scss';
 
 function SidePanel() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const listItemVariants = {
     visible: {
@@ -63,64 +75,61 @@ function SidePanel() {
 
   return (
     <Box flex="1 1 30%">
-      <Button  onClick={onOpen}>
-        Choose genre
-      </Button>
-      <Drawer
-        isOpen={isOpen}
-        placement='left'
-        onClose={onClose}
-      >
+      <Button onClick={onOpen}>Choose genre</Button>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
         <DrawerOverlay />
-        <DrawerContent background='black'>
+        <DrawerContent background="black">
           <DrawerCloseButton />
-          <DrawerBody p='20px 15px'>
-            <h2 className="text-2xl font-medium mb-2">Popular</h2>
-            <ul className="flex flex-col gap-1 text-xl mb-5">
-              <li className="cursor-pointer">
-                <NavLink
-                  to="/"
-                  style={({ isActive }) => (isActive ? { color: '#6d28d9' } : { color: 'white' })}
-                  className="flex items-center gap-3"
-                >
-                  Popular games
-                </NavLink>
-              </li>
-            </ul>
-            <h2 className="text-2xl font-medium mb-2">
+          <DrawerBody p="20px 15px">
+            <h2 className="sidepanel__title">Popular</h2>
+            <NavLink
+              to="/"
+              style={({ isActive }) => (isActive ? { color: '#6d28d9' } : { color: 'white' })}
+              className="sidepanel__link"
+            >
+              Popular games
+            </NavLink>
+
+            <h2 className="sidepanel__title">
               <Link to="/genres">Genres</Link>
             </h2>
             <AnimatePresence>
-              <ul className="flex flex-col gap-1 text-xl w-64">
+              <ul className="sidepanel__genres">
                 {genres.length > 0
                   ? genres.map((genre, i: number) => (
-                    <motion.li
-                      key={genre.id}
-                      className="cursor-pointer p-1"
-                      variants={listItemVariants}
-                      initial="hidden"
-                      animate="visible"
-                    >
-                      <NavLink
-                        to={`/games/${genre.slug}`}
-                        style={({ isActive }) => (isActive ? { color: '#6d28d9' } : { color: 'white' })}
-                        className="flex items-center gap-3"
+                      <motion.li
+                        key={genre.id}
+                        style={{ cursor: 'pointer', padding: '3px' }}
+                        variants={listItemVariants}
+                        initial="hidden"
+                        animate="visible"
                       >
-                        <Flex alignItems="center" justifyContent="center" bg="#27272a" p="5px">
-                          {genresIcons[i] ? genresIcons[i] : null}
-                        </Flex>
-                        {genre.name}
-                      </NavLink>
-                    </motion.li>
-                  ))
+                        <NavLink
+                          to={`/games/${genre.slug}`}
+                          style={({ isActive }) => (isActive ? { color: '#6d28d9' } : { color: 'white' })}
+                          className="sidepanel__link"
+                        >
+                          <Flex alignItems="center" justifyContent="center" bg="#27272a" p="5px">
+                            {genresIcons[i] ? genresIcons[i] : null}
+                          </Flex>
+                          {genre.name}
+                        </NavLink>
+                      </motion.li>
+                    ))
                   : null}
               </ul>
             </AnimatePresence>
           </DrawerBody>
-          <DrawerFooter >
-            <Button variant='outline' color='white' _hover={{
-              background: 'black'
-            }} mr={3} onClick={onClose}>
+          <DrawerFooter>
+            <Button
+              variant="outline"
+              color="white"
+              _hover={{
+                background: 'black',
+              }}
+              mr={3}
+              onClick={onClose}
+            >
               Cancel
             </Button>
           </DrawerFooter>
