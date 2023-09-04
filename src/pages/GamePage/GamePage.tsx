fc3169d605caad5fe676fbe7994a4b39274b97b0
Andrey <antonov.andrey.10101@gmail.com>
2023-09-04 12:44:21 +0300

    feat: add GameCard

--- src/pages/GamePage/GamePage.tsx
+++ src/pages/GamePage/GamePage.tsx
@@ -1,16 +1,7 @@
 import { useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Box, Image } from '@chakra-ui/react';
-import { Link, useParams } from 'react-router-dom';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { MdKeyboardBackspace } from 'react-icons/md';
-import { SiXbox, SiPlaystation3, SiPlaystation4, SiPlaystation5, SiNintendoswitch, SiWindows } from 'react-icons/si';
-import { AiOutlineQuestionCircle } from 'react-icons/ai';
+import { useParams } from 'react-router-dom';
 import {
   fetchGame,
   fetchScreenshots,
@@ -32,35 +23,12 @@ import GameSeries from '../../components/GameSeries/GameSeries';
 import GameInfo from '../../components/GameInfo/GameInfo';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
 import { useAppSelector, useAppDispatch } from '../../hooks/useRedux';
+import GameCard from '../../components/GameCard/GameCard';
 import './GamePage.scss';
-import { IScreenshot } from '../../types';
 
 function GamePage() {
   const { gameId } = useParams();
   const { getGame, getGameScreenshots, getGameAchievements, getGameAdditions, getListOfGamesSeries, getData } = RAWG();
-
-  function choosePlatformIcon(platformName: string) {
-    switch (platformName) {
-      case 'PlayStation 3':
-        return <SiPlaystation3 size="23" title={platformName} />;
-      case 'PlayStation 4':
-        return <SiPlaystation4 size="23" title={platformName} />;
-      case 'PlayStation 5':
-        return <SiPlaystation5 size="23" title={platformName} />;
-      case 'Xbox Series S/X':
-        return <SiXbox size="23" title={platformName} />;
-      case 'Xbox One':
-        return <SiXbox size="23" title={platformName} />;
-      case 'Xbox 360':
-        return <SiXbox size="23" title={platformName} />;
-      case 'Nintendo Switch':
-        return <SiNintendoswitch size="23" title={platformName} />;
-      case 'PC':
-        return <SiWindows size="23" title={platformName} />;
-      default:
-        return <AiOutlineQuestionCircle size="23" title={platformName} />;
-    }
-  }
 
   const {
     currentGame,
@@ -72,7 +40,6 @@ function GamePage() {
     gamesOfSeries,
     countGamesOfSeries,
   } = useAppSelector((state) => state.currentGame);
-  const { screenshots } = useAppSelector((state) => state.currentGame);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
@@ -81,7 +48,6 @@ function GamePage() {
       dispatch(fetchGame(getGame(gameId)));
       dispatch(fetchScreenshots(getGameScreenshots(gameId)));
     }
-
   }, [gameId]);
 
   function loadSection(tabIndex: number) {
@@ -124,45 +90,7 @@ function GamePage() {
       <main>
         {currentGame ? (
           <>
-            <div className="flex justify-between items-center mb-8">
-              <h2 className="text-2xl self-end ease-in duration-200 hover:text-violet-700">
-                <Link to="/" className="flex gap-2 items-center">
-                  <MdKeyboardBackspace size="40" />
-                  Store
-                </Link>
-              </h2>
-              <Box alignSelf="flex-end" display="flex" gap="15px" flexDirection="column" alignItems="flex-end">
-                <h1 className="text-5xl mb-4 text-right">{currentGame.name}</h1>
-                <Box display="inline-flex" gap="20px" alignItems="center" justifyContent="flex-end">
-                  {currentGame.platforms.map((platformItem) => (
-                    <h3 key={platformItem.platform.id}>{choosePlatformIcon(platformItem.platform.name)}</h3>
-                  ))}
-                </Box>
-              </Box>
-            </div>
-
-            <div className="flex gap-10 mb-20">
-              <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y]}
-                allowTouchMove={false}
-                navigation
-                pagination={{ clickable: true }}
-                className="GamePageSwiper"
-              >
-                <SwiperSlide>
-                  <Image src={currentGame.background_image} alt={currentGame.background_image} />
-                </SwiperSlide>
-                {screenshots.length > 0
-                  ? screenshots.map((screenshot: IScreenshot) => (
-                      <SwiperSlide key={screenshot.id}>
-                        <Image src={screenshot.image} alt={`Screenshot from ${currentGame.name}`} />
-                      </SwiperSlide>
-                    ))
-                  : null}
-              </Swiper>
-
-              <p className="bg-zinc-800 p-10 rounded-lg max-w-xl">{currentGame.description_raw}</p>
-            </div>
+            <GameCard currentGame={currentGame} />
             <Tabs variant="solid-rounded" onChange={(tabIndex) => loadSection(tabIndex)}>
               <TabList>
                 <Tab _selected={{ color: 'white', bg: 'purple.600' }}>Game Info </Tab>
